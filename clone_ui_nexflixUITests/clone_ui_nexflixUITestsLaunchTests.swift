65eab537b72ae5ff16a6c652da5f2214075e47c4
vandoannguyen <vandoannguyenhaui@gmail.com>
2022-06-09 08:37:40 +0700

    init project

---
+++ clone_ui_nexflixUITests/clone_ui_nexflixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  clone_ui_nexflixUITestsLaunchTests.swift
+//  clone_ui_nexflixUITests
+//
+//  Created by ATIAdmin on 09/06/2022.
+//
+
+import XCTest
+
+class clone_ui_nexflixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
